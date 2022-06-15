831a9aee9460b4399451eceb92c76c35b5c4a0b7
燕博文 <349952469@qq.com>
2022-06-15 09:36:42 +0800

    feat(projects): 去除unocss

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue';
 import { setupRouter } from './router'
 import App from './App.vue';
-import 'uno.css'
 
 const app = createApp(App)
 
--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,14 @@
 import vue from "@vitejs/plugin-vue";
-import Unocss from 'unocss/vite'
-import { presetAttributify } from 'unocss'
-import Components from 'unplugin-vue-components/vite'
+import Components from 'unplugin-vue-components/vite';
+import { VantResolver } from 'unplugin-vue-components/resolvers';
 
-import {
-  VantResolver,
-} from 'unplugin-vue-components/resolvers'
 export default {
   plugins: [
     vue(),
     
     Components({
       dts: 'src/typings/components.d.ts',
-      extensions: ['vue', 'tsx'],
-      /** 搜索子目录  */
-      deep: true,
-      /** 允许子目录作为组件的命名空间前缀 */
-      directoryAsNamespace: false,
-    resolvers:[ VantResolver() ]}),
-    Unocss({
-      presets: [
-        presetAttributify({ }),
-      ],
-    })
+      resolvers:[ VantResolver() ]}),
   ],
   
 };
